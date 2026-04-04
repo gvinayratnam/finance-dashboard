c14ca1a93ea17c9bd28280f93975a4151e9eaac4
gvinayratnam <vinaynani1229@gmail.com>
2026-04-04 14:45:50 +0530

    nav created with responsive in all screens

--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -12,6 +12,7 @@ import { useFilters } from "../../hooks/useFilters";
 import { calculateSummary } from "../../utils.js/calculations";
 import { useTheme } from "../../hooks/useTheme";
 import { FiChevronDown, FiUser } from "react-icons/fi";
+import { IoSunny, IoSunnyOutline } from "react-icons/io5";
 
 const emptyFrom = {
   title: "", amount: "", category: "", type: "expense",
@@ -43,61 +44,67 @@ const Dashboard = () => {
 
   return (
     <div className=" min-h-screen bg-white dark:bg-gray-900 text-gray-900 ">
-      <div className="flex justify-between items-center mb-6 border-b border-b-gray-200 dark:border-b-gray-600 px-4 py-4">
 
-        <div className="flex items-center gap-2 ">
-          <p className="bg-indigo-600 text-3xl text-white font-semibold h-13 w-13 rounded-2xl flex items-center justify-center ">F</p>
+      <nav className="flex justify-between items-center border-b border-b-gray-200 dark:border-b-gray-600 container-p sm:py-5 py-3">
+        
+        {/* left */}
+        <div className="flex items-center sm:gap-2 gap-1">
+          <p className="bg-indigo-600 sm:text-3xl text-lg text-white font-semibold sm:h-13 sm:w-13 h-7 w-7 sm:rounded-2xl rounded-lg flex items-center justify-center ">F</p>
           <div>
-            <h1 className="text-2xl font-semibold dark:text-white">FinanceHub</h1>
-            <p className="text-gray-400 text-xs">Financial Dashboard</p>
+            <h1 className="sm:text-2xl text-lg font-medium sm:font-semibold dark:text-white">FinanceHub</h1>
+            <p className="text-gray-400 text-xs sm:block hidden ">Financial Dashboard</p>
           </div>
+          
         </div>
 
+        {/* right */}
         <div className="flex gap-3 items-center">
-
 
           <div
             onClick={toggleTheme}
-            className="cursor-pointer px-4 py-2 rounded-xl border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-800 transition hover:bg-gray-100 dark:hover:bg-gray-600/95"
+            className="cursor-pointer sm:p-2 p-1 md:rounded-2xl sm:rounded-xl rounded-lg border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-800 transition duration-300 hover:bg-gray-100 dark:hover:bg-gray-600/95"
           >
             <span className="text-sm font-medium text-gray-700 dark:text-gray-200">
-              {isDark ? "Dark Mode" : "Light Mode"}
+            
+              <IoSunny  className={` sm:text-3xl text-lg transition-transform duration-500 ${isDark? "rotate-90 " : "rotate-0" }`}/>
             </span>
+
           </div>
 
           {/* role dropdown */}
-          <div className="flex items-center gap-1 bg-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600 dark:bg-gray-700 rounded-xl ">
+          <div className="flex items-center gap-1 bg-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600 dark:bg-gray-700 sm:rounded-xl rounded-lg">
             <Select
               value={role}
               suffixIcon={
                 <FiChevronDown className="text-gray-600 dark:text-gray-300" />
               }
               onChange={(value) => setRole(value)}
-              style={{ width: 170 }}
+              className="sm:w-42 "
               variant="borderless"
+              dropdownMatchSelectWidth={false} 
               popupClassName="custom-select-dropdown"
               labelRender={({ value }) => (
-                <div className="flex leading-tight gap-2 py-1 px-1 items-center">
-                  <div className="bg-indigo-400 rounded-full w-8 h-8 flex justify-center items-center">
-                    <FiUser className="text-lg text-white" />
+                <div className="flex leading-tight sm:gap-2 gap-1 py-1 sm:px-1 px-0 items-center">
+                  <div className="sm:bg-indigo-400 rounded-full sm:w-8 sm:h-8 flex justify-center items-center">
+                    <FiUser className="sm:text-lg text-base  sm:text-white text-indigo-400" />
                   </div>
                   <div className="flex flex-col">
-                    <span className="font-medium dark:text-white capitalize">{value}</span>
-                    <span className="text-xs text-gray-500 dark:text-gray-400">Active Role</span>
+                    <span className="sm:inline hidden font-medium dark:text-white capitalize">{value}</span>
+                    <span className="sm:inline hidden text-xs text-gray-500 dark:text-gray-400">Active Role</span>
                   </div>
                 </div>
               )}
               options={[
                 {
                   value: "viewer", label: <div className=" ">
-                    <p className="font-medium  capitalize ">Viewer</p>
-                    <p className="text-xs text-gray-400">Read-Only</p>
+                    <p className="font-medium capitalize ">Viewer</p>
+                    <p className="text-xs text-gray-400 dark:text-gray-600">Read-Only</p>
                   </div>
                 },
                 {
                   value: "admin", label: <div className="">
                     <p className="font-medium  capitalize ">Admin</p>
-                    <p className="text-xs text-gray-400">Full access</p>
+                    <p className="text-xs text-gray-400 dark:text-gray-600">Full access</p>
                   </div>
                 },
               ]}
@@ -105,9 +112,11 @@ const Dashboard = () => {
           </div>
 
         </div>
-      </div>
+      </nav>
 
-      <hr />
+      <div>
+
+      </div>
 
       {/* <SummaryCards balance={balance} totalIncome={totalIncome} totalExpense={totalExpense} /> */}
 
